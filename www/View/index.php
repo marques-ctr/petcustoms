43fa561fb8e7a68294224a7b349618311149f7b0
Carol <carolkitten3@hotmail.com>
2020-01-14 14:37:43 -0300

    teste

--- www/View/index.php
+++ www/View/index.php
@@ -1,6 +1,4 @@
 <?php include "view/header.php"?>
-
-
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="margin-top: 50px;">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
